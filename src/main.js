f20cf546271f757e952719ab820f836bbb48f41c
leesoohyuk29 <mutoo199g@gmail.com>
2023-10-02 18:44:48 +0800

    feat：优化element引入，处理版本不一致导致的构建问题

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
-import installElementPlus from './plugins/element'
+// import installElementPlus from './plugins/element'
 
 // 全局引入防抖函数
 // import debounce from './mixins/debounce';
 
 const app = createApp(App)
-installElementPlus(app)
+// installElementPlus(app)
+app.use(ElementPlus)
 // app.mixin(debounce);
 app.mount('#app')
